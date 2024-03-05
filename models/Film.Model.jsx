6810fd6199caf02b153de74f9ef3f46ce59d3cda
Aron <emmanuel.aron@gmail.com>
2024-03-05 15:47:13 +0100

    Film model and auth.routes.js

---
+++ models/Film.Model.jsx
@@ -0,0 +1,18 @@
+const mongoose = require('mongoose');
+
+const filmSchema = new mongoose.Schema({
+  title: {
+    type: String,
+    required: true
+  },
+  description: {
+    type: String,
+    required: true
+  },
+  videoLink: {
+    type: String,
+    required: true
+  }
+});
+
+const Film = mongoose.model('Film', filmSchema);
