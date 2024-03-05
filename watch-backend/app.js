2ae1d67a6323fe36ff8579d0695983aa2bc19639
Aron <emmanuel.aron@gmail.com>
2024-03-05 14:37:07 +0100

    First commit

---
+++ watch-backend/app.js
@@ -0,0 +1,35 @@
+// ℹ️ Gets access to environment variables/settings
+// https://www.npmjs.com/package/dotenv
+require("dotenv").config();
+
+// ℹ️ Connects to the database
+require("./db");
+
+// Handles http requests (express is node js framework)
+// https://www.npmjs.com/package/express
+const express = require("express");
+
+const app = express();
+
+// ℹ️ This function is getting exported from the config folder. It runs most pieces of middleware
+require("./config")(app);
+
+// 👇 Start handling routes here
+const indexRoutes = require("./routes/index.routes");
+app.use("/api", indexRoutes);
+
+const authRoutes = require("./routes/auth.routes");
+app.use("/auth", authRoutes);
+
+// ❗ To handle errors. Routes that don't exist or errors that you handle in specific routes
+require("./error-handling")(app);
+
+// Connect to DB
+mongoose
+    .connect("mongodb://127.0.0.1:27017/project-management-server")
+    .then((response) => {
+        console.log(`Connected! Database Name: "${response.connections[0].name}"`);
+    })
+    .catch((err) => console.error("Error connecting to Mongo", err));
+
+module.exports = app;
