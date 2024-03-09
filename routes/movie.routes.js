e94f4c7861cc160e2a77b3bba1e970b8fbd75f13
João Dias <152170826+jvfd1983@users.noreply.github.com>
2024-03-09 13:27:44 +0000

    Update Movie Routes

--- routes/movie.routes.js
+++ routes/movie.routes.js
@@ -14,14 +14,13 @@ router.get("/search", async (req, res, next) => {
 
     res.json({ movies });
   } catch (error) {
-    console.error("Erro ao pesquisar filmes:", error);
-    res.status(500).json({ message: "Erro interno do servidor" });
+    console.error("Error searching movies:", error);
+    res.status(500).json({ message: "Internal Server Error" });
   }
 });
 
 // GET "/api/movies" => Route to list all available movies
 router.get("/movies", (req, res, next) => {
-//router.get("/", (req, res, next) => {
   Movie.find({})
     .then((moviesFromDB) => res.status(200).json(moviesFromDB))
     .catch((err) => {
@@ -30,7 +29,6 @@ router.get("/movies", (req, res, next) => {
 });
 
 router.post("/movies", (req, res, next) => {
- // router.post("/", (req, res, next) => {
   Movie.create(req.body)
     .then((createdMovie) => {
       res.status(200).json(createdMovie);
